1c991dcb3c88a40d1983db5367d66ca0c1fbf271
shiakaze <quanhwu@gmail.com>
2015-01-28 15:50:35 -0800

    removed /Library/** from being tracked because it doesnt need to be

---
+++ timetravelcg/Assets/Script/NetworkScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkScript : MonoBehaviour {
+
+	public void Update() {
+		float send = Input.GetAxis("Horizontal");
+		if(send != 0) {
+			networkView.RPC("printText", RPCMode.All, send.ToString());
+		}
+	}
+
+	// test rpc function
+	[RPC]
+	public void printText(string text) {
+		Debug.Log(text);
+	}
+}
