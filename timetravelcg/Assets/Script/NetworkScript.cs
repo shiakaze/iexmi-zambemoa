22eaa96d94d2a2e91e9a0c7caae85739dca000b3
shiakaze <quanhwu@gmail.com>
2015-01-28 17:14:18 -0800

    rpc test works

--- timetravelcg/Assets/Script/NetworkScript.cs
+++ timetravelcg/Assets/Script/NetworkScript.cs
@@ -3,16 +3,29 @@ using System.Collections;
 
 public class NetworkScript : MonoBehaviour {
 
-	public void Update() {
-		float send = Input.GetAxis("Horizontal");
-		if(send != 0) {
-			networkView.RPC("printText", RPCMode.All, send.ToString());
+	public Transform testObj;
+
+	void Start() {
+		bool useNat = !Network.HavePublicAddress();
+		NetworkConnectionError result = Network.InitializeServer(32, 60000, useNat);
+		if (result != NetworkConnectionError.NoError) {
+			Network.Connect("127.0.0.1", 60000);
+			Debug.Log("after connection");
+			Debug.Log(Network.connections.ToString());
 		}
 	}
 
+	void Update() {
+		float send = Input.GetAxis("Horizontal");
+		if(send != 0) {
+			networkView.RPC("moveThatCube", RPCMode.OthersBuffered, send);
+			Debug.Log("Moving the other cube");
+		}
+	}
+	
 	// test rpc function
 	[RPC]
-	public void printText(string text) {
-		Debug.Log(text);
+	public void moveThatCube(float movement) {
+		testObj.Translate(movement,0,0);
 	}
 }
